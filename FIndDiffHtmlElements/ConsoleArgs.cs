894062e3c8c99a7e7d62a02ad42ad0c7df151176
Oleg Filimonov <Oleg.Filimonov@survalyzer.com>
2019-07-01 01:46:20 +0300

    init commit

---
+++ FIndDiffHtmlElements/ConsoleArgs.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FIndDiffHtmlElements
+{
+    internal class ConsoleArgs
+    {
+        private const string DefaultDirectory = "Samples";
+        private const string DefaultTargetElementId = "make-everything-ok-button";
+
+        public ConsoleArgs(string[] args)
+        {
+            var files = Directory.GetFiles(DefaultDirectory, "*.html");
+            OriginalFilePath = files.FirstOrDefault(f => f.Contains("origin"));
+            DiffFilePath = files.FirstOrDefault(f => f.Contains("diff"));
+            TargetElementId = DefaultTargetElementId;
+
+            if (args == null || args.Length == 0)
+                return;
+
+            var argsList = args.ToList();
+            GetOption(argsList, "-id", value => TargetElementId = value);
+            GetOption(argsList, "-o", value => OriginalFilePath = value);
+            GetOption(argsList, "-d", value => DiffFilePath = value);
+        }
+
+        private void GetOption(List<string> argsList, string key, Action<string> processAction)
+        {
+            var idIndex = argsList.FindIndex(i => i.Equals(key, StringComparison.OrdinalIgnoreCase)) + 1;
+            if (idIndex < argsList.Count)
+            {
+                processAction(argsList[idIndex]);
+                argsList.RemoveAt(idIndex);
+                argsList.RemoveAt(idIndex - 1);
+            }
+        }
+
+        public bool IsValid =>
+            !string.IsNullOrEmpty(OriginalFilePath)
+            && !string.IsNullOrEmpty(DiffFilePath)
+            && File.Exists(OriginalFilePath)
+            && File.Exists(DiffFilePath);
+
+        public string OriginalFilePath { get; private set; }
+
+        public string DiffFilePath { get; private set; }
+
+        public string TargetElementId { get; private set; }
+    }
+}
--- FIndDiffHtmlElements/Program.cs
+++ FIndDiffHtmlElements/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace FIndDiffHtmlElements
 {
@@ -10,6 +11,83 @@ namespace FIndDiffHtmlElements
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var consoleArgs = new ConsoleArgs(args);
+                if (!consoleArgs.IsValid)
+                {
+                    Console.WriteLine($"Invalid parameters: {nameof(consoleArgs.OriginalFilePath)} - {consoleArgs.OriginalFilePath};" +
+                                      $" {nameof(consoleArgs.DiffFilePath)} - {consoleArgs.DiffFilePath};" +
+                                      $" {nameof(consoleArgs.TargetElementId)} - {consoleArgs.TargetElementId}");
+                    return;
+                }
+
+                var element = GetOriginElement(consoleArgs);
+                if (element == null)
+                {
+                    Console.WriteLine("Can't find origin element");
+                }
+                else
+                {
+                    Console.WriteLine("Origin Element: ");
+                    ShowElement(element);
+                }
+
+                var diffDocument = new HtmlDocument();
+                diffDocument.Load(consoleArgs.DiffFilePath);
+                var diffElement = diffDocument.GetElementbyId(consoleArgs.TargetElementId) ?? GetDiffElement(diffDocument, element);
+
+                if (diffElement == null)
+                    Console.WriteLine("Can't find diff element.");
+                else
+                {
+                    Console.WriteLine("Diff element: ");
+                    ShowElement(diffElement);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error trying to find element by id, Message: {0}", ex.Message);
+            }
+        }
+
+        private static HtmlNode GetDiffElement(HtmlDocument diffDocument, HtmlNode element)
+        {
+            var allElements = diffDocument.DocumentNode.SelectNodes($"//{element.Name}");
+            return allElements?.Select(i => new { Element = i, Rate = CalcRate(element, i) }).OrderByDescending(i => i.Rate).FirstOrDefault()?.Element;
+        }
+
+        private static int CalcRate(HtmlNode originElement, HtmlNode diffElement)
+        {
+            return originElement.Attributes
+                .Select(attr => diffElement.Attributes
+                    .FirstOrDefault(a => string.Equals(a.Name, attr.Name, StringComparison.OrdinalIgnoreCase)
+                                         && string.Equals(a.Value, attr.Value, StringComparison.OrdinalIgnoreCase))).Count(diffAttr => diffAttr != null);
+        }
+
+        private static void ShowElement(HtmlNode element)
+        {
+            Console.WriteLine($"Successfully found. InnerText: {element.InnerText}");
+            var attributesData = string.Join(", ",
+                element.Attributes.Select(attribute => $"{attribute.Name} = {attribute.Value}"));
+            Console.WriteLine(attributesData);
+            Console.WriteLine($"Path: {GetElementPath(element)}");
+        }
+
+        private static string GetElementPath(HtmlNode element)
+        {
+            return element.ParentNode == null ? string.Empty : $"{GetElementPath(element.ParentNode)}>{element.Name}";
+        }
+
+        private static HtmlNode GetOriginElement(ConsoleArgs consoleArgs)
+        {
+            var document = new HtmlDocument();
+            document.Load(consoleArgs.OriginalFilePath);
+
+            var element = document.GetElementbyId(consoleArgs.TargetElementId);
+            if (element == null)
+                throw new Exception("Element not found");
+            return element;
         }
     }
 }
